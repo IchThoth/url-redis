791c38993ca24adc1eec1040bb7b4ee17c6cbbaf
ichthoth <shalomtata334@gmail.com>
2022-11-17 02:39:27 +0100

    created redis client (not tested)

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"context"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var Ctx = context.Background()
+
+//Creates a redis client instance
+func createClient(dbNo int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB:       dbNo,
+	})
+	return rdb
+}
