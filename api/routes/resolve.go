14ba00ddd79f9addd72a9195057a284ce99afdce
ichthoth <shalomtata334@gmail.com>
2022-11-29 11:50:12 +0100

    resolved url

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,32 @@
 package routes
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/ichthoth/url-redis/m/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found",
+		})
+	} else if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
